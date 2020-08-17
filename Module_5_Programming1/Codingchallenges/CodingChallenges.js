e51beaa3709cec94db6305a88cf2283b69829b0e
xicabambafunk <jayran@cca.edu>
2020-08-17 02:25:49 -0400

    last

--- Module_5_Programming1/Codingchallenges/CodingChallenges.js
+++ Module_5_Programming1/Codingchallenges/CodingChallenges.js
@@ -48,7 +48,7 @@ CHALLENGE 03
 var World01 = ["Wipe", "the", "sweat", "off", "my", "dome", "spit", "the", "phlegm", "on", "the", "streets", "Suede",
 "Timbs", "on", "my", "feets", "makes", "my", "cipher", "complete", "Whether", "cruising", "in", "a", "Sikh's", "cab", "or", "Montero", "Jeep", "I", "can't", "call", "it", "the", "beats", "make", "me", "falling", "asleep", "I", "keep" "falling", "but", "never", "falling", "six", "feet", "deep", "I'm", "out", "for", "presidents", "to", "represent", "me", "I'm", "out", "for", "presidents", "to", "represent", "me", "I'm", "out", "for", "dead", "presidents", "to", "represent", "me"]
 
-/* ^^^^^I was looking at ways to go through all that...... but I found a simpler way online with a g search & letter replacement online */
+/* ^^^^^I was looking at ways to go through all that...... but I found a simpler way online with a g search & letter replacement */
 
 
 
